26335f59d90e4ab1eb12d971cb4c1441b2938734
hyoideum <ivana.protic@ymail.com>
2025-03-21 16:25:29 +0100

    functionality achived

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,17 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello and welcome!");
+        Match m1 = new Match("Hrvatska", "Francuska");
+        Match m2 = new Match("Mexico", "Canada");
+        Match m3 = new Match("Italy", "urugvay");
+        Match m4 = new Match("Bih", "USA");
+        new Match("Japan", "Bolivia");
+
+        m1.setScore(1,0);
+        m2.setScore(3,1);
+        m3.setScore(0,2);
+        m3.finishMatch();
+        m4.setScore(0,1);
+        m1.PrintMatches();
     }
 }
--- src/Match.java
+++ src/Match.java
@@ -1,16 +1,32 @@
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
 
 public class Match {
+    public enum Status {
+        ACTIVE,
+        FINISHED
+    }
+
+    private static List<Match> matchList = new ArrayList<>();
+
     private String home;
     private String away;
     private int scoreHome;
     private int scoreAway;
+    private LocalDateTime matchStart;
+    private Status status;
 
     public Match(String home, String away) {
         this.home = home;
         this.away = away;
         this.scoreHome = 0;
         this.scoreAway = 0;
+        this.matchStart = LocalDateTime.now();
+        this.status = Status.ACTIVE;
+
+        matchList.add(this);
     }
 
     public String getHome() {
@@ -29,15 +45,46 @@ public class Match {
         return scoreAway;
     }
 
+    private int getTotalScore() {
+        return scoreHome + scoreAway;
+    }
+
+    public LocalDateTime getStart() {
+        return matchStart;
+    }
+
+    public List<Match> getMatchList() {
+        return matchList;
+    }
+
     public void setScore(int scoreHome, int scoreAway) {
         if(scoreHome < 0 || scoreHome > 99 || scoreAway < 0 || scoreAway > 99) {
             throw new IllegalArgumentException("Invalid score!");
         }
         this.scoreHome = scoreHome;
         this.scoreAway = scoreAway;
+        SortList();
+    }
+
+    public void finishMatch() {
+        this.status = Status.FINISHED;
+        matchList.remove(this);
+    }
+
+    private void SortList() {
+        matchList.sort(Comparator
+                .comparing(Match::getTotalScore)
+                .thenComparing(Match::getStart)
+                .reversed());
     }
 
     public void PrintMatch() {
-        System.out.print(home + " " + scoreHome + " - " + scoreAway + " " + away);
+        System.out.println(home + " " + scoreHome + " - " + scoreAway + " " + away);
+    }
+
+    public void PrintMatches() {
+        for(Match m : matchList) {
+            m.PrintMatch();
+        }
     }
 }
